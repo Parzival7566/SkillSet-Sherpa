d0c95100fba43fedc6e05d37f6800bfd0205382d
Parzival7566 <94829251+Parzival7566@users.noreply.github.com>
2023-09-18 08:54:20 +0530

    Update prompt_creation.py

--- backend/python/prompt_creation.py
+++ backend/python/prompt_creation.py
@@ -2,12 +2,16 @@
 with open("extracted_data.txt", "r") as extracted_data_file:
     extracted_data = extracted_data_file.read()
 
+with open("riasec_results.txt", "r") as riasec_data_file:
+    riasec_data = riasec_data_file.read()
+
 # Define the beginning and ending text
 beginning_text = "I am a student in high school and my board exam marks are:"
+middle_text = "I also took the Holland Occupational Themes or the RIASEC codes test to figure out the best career and got the following scores:"
 ending_text = "Based on my marks and subjects, can you suggest career paths that I could take?"
 
 # Create the content for the question.txt file
-question_content = f"{beginning_text}\n\n{extracted_data}\n\n{ending_text}"
+question_content = f"{beginning_text}\n\n{extracted_data}\n\n{middle_text}\n\n{riasec_data}\n\n{ending_text}"
 
 # Write the content to the question.txt file
 with open("question.txt", "w") as question_file:
