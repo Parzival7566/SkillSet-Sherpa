00a73204acf1fdb4d49f2a8ccf95cbbb9fdf4add
Parzival7566 <kanishkarya2811@gmail.com>
2023-09-19 14:00:36 +0530

    committed the files using git cli

--- project gen AI/app.py
+++ project gen AI/app.py
@@ -129,60 +129,70 @@ def run_text_processing():
         logging.error(f"Exception in text processing: {str(e)}")
 
 # Function to run RIASEC score calculation
+# Function to run RIASEC score calculation
 def run_riasec_score_calculation():
     try:
-        from collections import defaultdict
-
-        # Define the questions and corresponding RIASEC categories
+        import json
         questions = [
-            ("Do you enjoy working with your hands and tools?", "Realistic (R)"),
-            ("Are you naturally curious and enjoy exploring new ideas?", "Investigative (I)"),
-            ("Do you have a passion for creative activities like drawing, writing, or music?", "Artistic (A)"),
-            ("Do you find fulfillment in helping and working with others?", "Social (S)"),
-            ("Are you ambitious and enjoy taking on leadership roles?", "Enterprising (E)"),
-            ("Do you prefer working in structured and organized environments?", "Conventional (C)"),
-            ("Are you interested in working outdoors or in natural settings?", "General (Mixed RIASEC)"),
-            ("Do you prefer a fast-paced and dynamic work environment?", "General (Mixed RIASEC)"),
-            ("Do you enjoy working with machinery and technology?", "Realistic (R)"),
-            ("Do you like conducting experiments and solving complex problems?", "Investigative (I)"),
-            ("Do you have a talent for artistic and creative expression?", "Artistic (A)"),
-            ("Do you enjoy teaching or mentoring others?", "Social (S)"),
-            ("Do you have strong negotiation and persuasion skills?", "Enterprising (E)"),
-            ("Do you prefer a neat and structured workspace?", "Conventional (C)"),
-            ("Do you like spending time in nature and outdoor activities?", "General (Mixed RIASEC)"),
-            ("Do you thrive in high-pressure and competitive situations?", "General (Mixed RIASEC)"),
-            ("Are you mechanically inclined and good at fixing things?", "Realistic (R)"),
-            ("Do you enjoy analyzing data and conducting research?", "Investigative (I)"),
-            ("Are you skilled in playing musical instruments or creating art?", "Artistic (A)"),
-            ("Do you excel in teamwork and collaboration?", "Social (S)")
+        ("Do you enjoy working with your hands and tools?", "Realistic (R)"),
+        ("Are you naturally curious and enjoy exploring new ideas?", "Investigative (I)"),
+        ("Do you have a passion for creative activities like drawing, writing, or music?", "Artistic (A)"),
+        ("Do you find fulfillment in helping and working with others?", "Social (S)"),
+        ("Are you ambitious and enjoy taking on leadership roles?", "Enterprising (E)"),
+        ("Do you prefer working in structured and organized environments?", "Conventional (C)"),
+        ("Are you interested in working outdoors or in natural settings?", "General (Mixed RIASEC)"),
+        ("Do you prefer a fast-paced and dynamic work environment?", "General (Mixed RIASEC)"),
+        ("Do you enjoy working with machinery and technology?", "Realistic (R)"),
+        ("Do you like conducting experiments and solving complex problems?", "Investigative (I)"),
+        ("Do you have a talent for artistic and creative expression?", "Artistic (A)"),
+        ("Do you enjoy teaching or mentoring others?", "Social (S)"),
+        ("Do you have strong negotiation and persuasion skills?", "Enterprising (E)"),
+        ("Do you prefer a neat and structured workspace?", "Conventional (C)"),
+        ("Do you like spending time in nature and outdoor activities?", "General (Mixed RIASEC)"),
+        ("Do you thrive in high-pressure and competitive situations?", "General (Mixed RIASEC)"),
+        ("Are you mechanically inclined and good at fixing things?", "Realistic (R)"),
+        ("Do you enjoy analyzing data and conducting research?", "Investigative (I)"),
+        ("Are you skilled in playing musical instruments or creating art?", "Artistic (A)"),
+        ("Do you excel in teamwork and collaboration?", "Social (S)")
         ]
 
-        # Initialize a dictionary to store user responses
-        user_responses = defaultdict(int)
-
-        # Read the user's responses from a file
+        # Load user responses from the answers.txt file
         with open("answers.txt", "r") as file:
-            user_responses_list = [int(line.strip()) for line in file]
+            user_responses = json.load(file)
 
-        if len(user_responses_list) != len(questions):
-            logging.error("Number of responses does not match the number of questions.")
+        if not user_responses:
+            print("No responses found in the file.")
             return
 
-        for (question, category), response in zip(questions, user_responses_list):
+        if len(user_responses) != len(questions):
+            print("Number of responses does not match the number of questions.")
+            return
+
+        print("\nRIASEC Scores:")
+        total_score = sum(int(response) for response in user_responses.values())
+        user_scores = {}
+
+        for (question, category), response in zip(questions, user_responses.values()):
             category = category.split()[0]  # Extract the category abbreviation (e.g., "Realistic (R)" => "Realistic")
-            user_responses[category] += response
+            user_scores[category] = int(response)
+            print(f"{category}: {response}")
 
         # Normalize the scores to a total of 100
-        total_score = sum(user_responses.values())
-        normalized_scores = {category: (score / total_score) * 100 for category, score in user_responses.items()}
+        normalized_scores = {category: (score / total_score) * 100 for category, score in user_scores.items()}
 
-        # Save the results to a text file
+        print("\nNormalized RIASEC Scores (out of 100):")
+        for category, score in normalized_scores.items():
+            print(f"{category}: {score:.2f}%")
+
+        # Write the normalized RIASEC scores to riasec_results.txt
         with open("riasec_results.txt", "w") as file:
             file.write("RIASEC Scores:\n")
             for category, score in normalized_scores.items():
                 file.write(f"{category}: {score:.2f}%\n")
+
     except Exception as e:
         logging.error(f"Exception in RIASEC score calculation: {str(e)}")
+
 
 # Function to run prompt creation
 def run_prompt_creation():
@@ -262,13 +272,11 @@ def send_message():
         return jsonify({"response": str(e)})
 
 # Route to serve the aptitude test form
-@app.route("/aptitude-test", methods=["GET", "POST"])
+@app.route('/aptitude-test', methods=['POST'])
 def aptitude_test():
     try:
         data = request.get_json()
-        app.logger.info("Received data: %s", data)  # Log received data
-        answers = data.get("answers")
-
+        answers = data.get('answers')
         # Process and save answers here
         save_answers(answers)  # Save answers to a file or process as needed
         return jsonify({"success": True})  # Respond with success
@@ -276,8 +284,9 @@ def aptitude_test():
         app.logger.error("Error processing aptitude test form: %s", str(e))  # Log error
         return jsonify({"success": False, "error": str(e)})  # Respond with error
 
+
         return redirect(url_for("thank_you"))
-    return render_template("./index2.html")
+    return render_template("index2.html")
 
 @app.route("/thank_you")
 def thank_you():
